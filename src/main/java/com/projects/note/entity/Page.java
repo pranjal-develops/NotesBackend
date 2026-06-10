61f87d80fec4e9af41cc5520c1d862baf3cce48b
PranjalUpadhyay1 <pranjalupadhyay25@gmail.com>
2026-06-10 22:46:35 +0530

    Updated Pages

--- src/main/java/com/projects/note/entity/Page.java
+++ src/main/java/com/projects/note/entity/Page.java
@@ -3,8 +3,12 @@ package com.projects.note.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Map;
 
 @Entity
 @Getter
@@ -15,7 +19,22 @@ public class Page {
     private Long id;
     private String title;
     @Column(columnDefinition = "TEXT")
-    private String content;
+    private String contentHtml;
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb") // Use jsonb for PostgreSQL or json for MySQL
+    private List<String> drawings;
+    
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private List<Map<String, Object>> charts;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private List<Map<String, String>> codeBlocks;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private List<String> images;
     //    private int pageOrder; // Commented it since primitive int cannot be null, this giving error in DTO when pageOrder is not provided
     private Integer pageOrder;
     private OffsetDateTime createdDate;
